014f5deb85ca7113885a7efb46ef2e338abc68f2
Akshat Vishwakarma <a.vishwakarma1021@gmail.com>
2022-02-25 13:57:17 +0530

    some scripting done

---
+++ Questions/first.md
@@ -0,0 +1,63 @@
+### Questions for the first lesson
+
+### Fill ups
+
+## The \_\_\_ positioning helps to put the text fixed on the browser.
+
+# Abosolute
+
+# Static
+
+# Fixed
+
+# Relative
+
+## The \_\_\_ positioning is the default position for HTML element.
+
+# Abosolute
+
+# Static
+
+# Fixed
+
+# Relative
+
+## The \_\_\_ tag is placed between <head> tag.
+
+# <body>
+
+# <meta>
+
+# <h1>
+
+# <audio>
+
+## <input type="___"> defines a field for entering a URL.
+
+# color
+
+# image
+
+# link
+
+# URL
+
+## The \_\_\_ property sets background colour in your webpages.
+
+# background-color
+
+# bg-color
+
+# color
+
+# back-color
+
+## the \_\_\_ selector selects the id attribute of an HTML element to select a specific element.
+
+# ID
+
+# Class
+
+# Grouping
+
+# Universal
--- script/script.js
+++ script/script.js
@@ -1,15 +1,24 @@
 // First Stage
 const firstBtn = document.getElementById("startBtn");
 const rulesSection = document.querySelector(".rulesSection");
+const bg1 = document.querySelector(".bg1");
 const rulesCancelBtn = document.getElementById("rulesCancelBtn");
 
-// animattions
+// animations
 var rulesAnim = {
   animation: "rulesOpenAnim .3s ease-out forwards",
 };
 
 var rulesAnimReverse = {
   animation: "rulesCloseAnim .3s ease-out forwards",
+};
+
+var bg1Anim = {
+  animation: "bg1Anim .6s ease-out forwards",
+};
+
+var gameBgAnim = {
+  animation: "gameBgAnim .6s ease-out forwards",
 };
 
 // Actions
@@ -19,4 +28,58 @@ firstBtn.onclick = () => {
 
 rulesCancelBtn.onclick = () => {
   Object.assign(rulesSection.style, rulesAnimReverse);
+};
+
+// Making the timer
+const timeOutput = document.getElementById("timeOutput");
+const startingTime = 45;
+var time = startingTime * 60;
+
+setInterval(countDown, 1000);
+
+function countDown() {
+  var minutes = Math.floor(time / 60);
+  var seconds = time % 60;
+
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+  seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  timeOutput.innerHTML = `${minutes}m : ${seconds}s`;
+  time--;
+}
+
+// Starting the game
+const startBtn = document.getElementById("rulesStartBtn");
+const gameBg = document.querySelector(".gameBg");
+
+startBtn.onclick = () => {
+  Object.assign(bg1.style, bg1Anim);
+  Object.assign(rulesSection.style, bg1Anim);
+  Object.assign(gameBg.style, gameBgAnim);
+};
+
+// Game mechanics
+const nextBtn = document.getElementById("nextBtn");
+const Q1Options = document.querySelectorAll(".Q1Answers");
+const answer = 3;
+var Q1Value;
+
+Q1Options[0].onclick = () => {
+  Q1Value = 1;
+};
+
+Q1Options[1].onclick = () => {
+  Q1Value = 2;
+};
+
+Q1Options[2].onclick = () => {
+  Q1Value = 3;
+};
+
+Q1Options[3].onclick = () => {
+  Q1Value = 4;
+};
+
+nextBtn.onclick = () => {
+  answer == Q1Value ? console.log("correct") : console.log("incorrect");
 };
